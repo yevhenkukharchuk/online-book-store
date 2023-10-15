8697ec1fe1bc3817e2abdee4963e1f651c2b6a12
Yevhen Kukharchuk <jancker19@ukr.net>
2023-10-15 21:17:18 +0300

    Initial commit (infrastructure)

---
+++ src/main/java/yevhen/bookstore/OnlineBookStoreApplication.java
@@ -0,0 +1,12 @@
+package yevhen.bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBookStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineBookStoreApplication.class, args);
+    }
+}

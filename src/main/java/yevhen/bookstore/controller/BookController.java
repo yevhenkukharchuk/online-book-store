a793c84455e3e46987fc48476c1b9fd1be1bbc0b
Yevhen Kukharchuk <jancker19@ukr.net>
2023-11-08 22:52:55 +0200

    06 - added pagination, sorting and swagger ability

--- src/main/java/yevhen/bookstore/controller/BookController.java
+++ src/main/java/yevhen/bookstore/controller/BookController.java
@@ -1,8 +1,11 @@
 package yevhen.bookstore.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +21,7 @@ import yevhen.bookstore.dto.BookSearchParametersDto;
 import yevhen.bookstore.dto.CreateBookRequestDto;
 import yevhen.bookstore.service.BookService;
 
+@Tag(name = "Online book store managing", description = "Endpoints for managing book store")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/books")
@@ -25,22 +29,26 @@ public class BookController {
     private final BookService bookService;
 
     @GetMapping
-    public List<BookDto> getAll() {
-        return bookService.findAll();
+    @Operation(summary = "Get all books", description = "Get a list of all available books")
+    public List<BookDto> getAll(Pageable pageable) {
+        return bookService.findAll(pageable);
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Get one book by id", description = "Get one book by id")
     public BookDto getBookById(@PathVariable Long id) {
         return bookService.getBookById(id);
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "Create a new book", description = "Create a new book")
     public BookDto createBook(@RequestBody @Valid CreateBookRequestDto requestDto) {
         return bookService.save(requestDto);
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Update a book by id", description = "Update a book with certain id")
     public BookDto updateBookById(@RequestBody CreateBookRequestDto requestDto,
                                   @PathVariable Long id) {
         return bookService.updateBookById(requestDto, id);
@@ -48,11 +56,14 @@ public class BookController {
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{id}")
+    @Operation(summary = "Delete one book by id", description = "Delete one book by id")
     public void deleteBookById(@PathVariable Long id) {
         bookService.deleteBookById(id);
     }
 
     @GetMapping("/search")
+    @Operation(summary = "Filter books by parameters",
+            description = "Filter books by titles and authors")
     public List<BookDto> searchBooks(BookSearchParametersDto searchParameters) {
         return bookService.search(searchParameters);
     }
--- src/main/java/yevhen/bookstore/service/impl/BookServiceImpl.java
+++ src/main/java/yevhen/bookstore/service/impl/BookServiceImpl.java
@@ -2,6 +2,7 @@ package yevhen.bookstore.service.impl;
 
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import yevhen.bookstore.dto.BookDto;
@@ -28,8 +29,8 @@ public class BookServiceImpl implements BookService {
     }
 
     @Override
-    public List<BookDto> findAll() {
-        return bookRepository.findAll()
+    public List<BookDto> findAll(Pageable pageable) {
+        return bookRepository.findAll(pageable)
                 .stream()
                 .map(bookMapper::toDto)
                 .toList();

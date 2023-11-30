03158281275ecf465d76a5ab95fa5fc9c0fea252
Yevhen Kukharchuk <jancker19@ukr.net>
2023-11-30 14:55:09 +0200

    09.1 - fixed one mistake and updated approach to generate shopping cart id

--- src/main/java/yevhen/bookstore/model/ShoppingCart.java
+++ src/main/java/yevhen/bookstore/model/ShoppingCart.java
@@ -2,10 +2,8 @@ package yevhen.bookstore.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
+import jakarta.persistence.MapsId;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
@@ -20,11 +18,10 @@ import lombok.Setter;
 @Table(name = "shopping_carts")
 public class ShoppingCart {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @OneToOne(fetch = FetchType.LAZY, optional = false)
-    @JoinColumn(name = "user_id", nullable = false)
+    @MapsId
     private User user;
 
     @OneToMany(mappedBy = "shoppingCart")
